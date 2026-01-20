3a8c98c3aa01dfc8bc77403332a15d62b2bfb450
Kassim <kassim.mirza711@live.com>
2026-01-20 12:04:14 +0000

    initial commit

---
+++ CourtSide/CourtSide Watch App/CourtSideApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourtSideApp.swift
+//  CourtSide Watch App
+//
+//  Created by Kassim Mirza on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct CourtSide_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CourtSide/CourtSide Watch AppTests/CourtSide_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CourtSide_Watch_AppTests.swift
+//  CourtSide Watch AppTests
+//
+//  Created by Kassim Mirza on 20/01/2026.
+//
+
+import Testing
+@testable import CourtSide_Watch_App
+
+struct CourtSide_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
